82311262a11c7d4f4cdcc4adbd9ecc971929ddf3
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-09-03 00:28:24 +0200

    Add stubs for old options

--- opencanary/cli.py
+++ opencanary/cli.py
@@ -4,10 +4,21 @@ import click
 ENV_VAR_PREFIX = 'OPENCANARY_'
 
 
-@click.group()
+copy_config = run_dev_app = start_app = stop_app = run_user_module = (
+    lambda *a, **kw: print({'a': a, 'kw': kw})
+)
+
+
+@click.command()
 @click.version_option()
+@click.option('--copyconfig', 'action', flag_value=copy_config)
+@click.option('--dev', 'action', flag_value=run_dev_app)
+@click.option('--start', 'action', flag_value=start_app)
+@click.option('--stop', 'action', flag_value=stop_app)
+@click.option('--usermodule', 'action', flag_value=run_user_module)
 @click.pass_context
-def cli(ctx, **kwargs):
+def cli(ctx, action, **kwargs):
+    action(ctx, action, **kwargs)
     ctx.obj.update(**kwargs)
 
 
