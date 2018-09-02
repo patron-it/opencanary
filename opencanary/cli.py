342de9f48a5dd43e5fc1b005eb287bc237426e82
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-09-03 00:07:32 +0200

    Add basic Click CLI app with version option
    Ref #8

--- opencanary/cli.py
+++ opencanary/cli.py
@@ -1,6 +1,18 @@
 import click
 
+
+ENV_VAR_PREFIX = 'OPENCANARY_'
+
+
+@click.group()
+@click.version_option()
+@click.pass_context
+def cli(ctx, **kwargs):
+    ctx.obj.update(**kwargs)
+
+
 def main():
-    pass
+    return cli(obj={}, auto_envvar_prefix=ENV_VAR_PREFIX)
+
 
 __name__ == '__main__' and main()
