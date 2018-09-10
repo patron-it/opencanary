32c0d65858ef8805fdce9f99c37650a51fb796a1
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-09-10 02:51:43 +0200

    Implement copyconfig CLI option

--- opencanary/cli.py
+++ opencanary/cli.py
@@ -1,4 +1,7 @@
 import os
+from pkg_resources import resource_filename
+import shutil
+import textwrap
 
 import click
 
@@ -6,9 +9,34 @@ ENV_VAR_PREFIX = 'OPENCANARY_'
 USER_CONFIG_PATH = os.path.join(os.path.expanduser("~"), ".opencanary.conf")
 
 
-copy_config = run_dev_app = start_app = stop_app = run_user_module = (
-    lambda *a, **kw: print({'a': a, 'kw': kw})
+run_dev_app = start_app = stop_app = run_user_module = (
+    lambda ctx: print({'ctx': ctx})
 )
+
+
+def config_exists(conf_path):
+    return os.path.exists(conf_path)
+
+
+def copy_config(ctx):
+    if config_exists(USER_CONFIG_PATH):
+        click.echo(
+            "[e] A config file already exists at {}, please move it first".
+            format(USER_CONFIG_PATH)
+        )
+        ctx.exit(1)
+
+    default_conf = resource_filename('opencanary', 'data/settings.json')
+    shutil.copy(default_conf, USER_CONFIG_PATH)
+    click.echo(
+        textwrap.dedent(
+            '''
+            [*] A sample config file is ready ({conf})
+            [*] Edit your configuration, then launch with "{cmd} --start"
+            '''.
+            format(conf=USER_CONFIG_PATH, cmd=ctx.command_path)
+        ).strip()
+    )
 
 
 @click.command()
@@ -40,7 +68,7 @@ copy_config = run_dev_app = start_app = stop_app = run_user_module = (
 )
 @click.pass_context
 def cli(ctx, action, **kwargs):
-    action(ctx, action, **kwargs)
+    action(ctx)
     ctx.obj.update(**kwargs)
 
 
