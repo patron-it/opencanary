b36911745be53617e378174e07786988d5e7e1b6
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-09-10 02:17:06 +0200

    Add CLI option hints

--- opencanary/cli.py
+++ opencanary/cli.py
@@ -1,7 +1,9 @@
+import os
+
 import click
 
-
 ENV_VAR_PREFIX = 'OPENCANARY_'
+USER_CONFIG_PATH = os.path.join(os.path.expanduser("~"), ".opencanary.conf")
 
 
 copy_config = run_dev_app = start_app = stop_app = run_user_module = (
@@ -11,11 +13,31 @@ copy_config = run_dev_app = start_app = stop_app = run_user_module = (
 
 @click.command()
 @click.version_option()
-@click.option('--copyconfig', 'action', flag_value=copy_config)
-@click.option('--dev', 'action', flag_value=run_dev_app)
-@click.option('--start', 'action', flag_value=start_app)
-@click.option('--stop', 'action', flag_value=stop_app)
-@click.option('--usermodule', 'action', flag_value=run_user_module)
+@click.option(
+    '--copyconfig', 'action',
+    help='Create a default config file at {}'.format(USER_CONFIG_PATH),
+    flag_value=copy_config,
+)
+@click.option(
+    '--dev', 'action',
+    help='Run the opencanaryd process in the foreground',
+    flag_value=run_dev_app,
+)
+@click.option(
+    '--start', 'action',
+    help='Start the opencanaryd process',
+    flag_value=start_app,
+)
+@click.option(
+    '--stop', 'action',
+    help='Stop the opencanaryd process',
+    flag_value=stop_app,
+)
+@click.option(
+    '--usermodule', 'action',
+    help='Run opencanaryd in foreground with only usermodules enabled',
+    flag_value=run_user_module,
+)
 @click.pass_context
 def cli(ctx, action, **kwargs):
     action(ctx, action, **kwargs)
